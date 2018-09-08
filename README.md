dcb5db6dcebdbe0576719528375ee1a69d6701f2
Usheninte <jrandangana@gmail.com>
2018-09-08 04:20:09 +0100

    renamed plugin

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # [By Usheninte](https://twitter.com/Usheninte)
 
-This is a React plugin for the freeCodeCamp Test Suite
+This is a React plugin for the freeCodeCamp Test Suite, built in **2018**.
+
+
+
+##### This [StackOverflow link](https://stackoverflow.com/questions/9659265/check-if-javascript-script-exists-on-page) was very helpful in guiding me with building the [test.js](https://github.com/Usheninte/reactestjs/blob/master/test/test.js) file
--- test/test.js
+++ test/test.js
@@ -1,15 +1,13 @@
 var assert = require('assert');
-describe('reactestjs', function() {
-  describe('fCC_TestSuite', function() {
-    it('should check for fCC test suite link', function() {
-      var url = "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
-      if (!url) {
-        var scripts = document.getElementsByTagName('script');
-        for (var i = scripts.length; i--;) {
-            if (scripts[i].src == url) return true;
-        }
-        return false;
-      };
-    });
+describe('react-fcctest', function() {
+  it('should check for fCC Test Suite script', function() {
+    var url = "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
+    if (!url) {
+      var scripts = document.getElementsByTagName('script');
+      for (var i = scripts.length; i--;) {
+          if (scripts[i].src == url) return true;
+      }
+      return false;
+    };
   });
 });
