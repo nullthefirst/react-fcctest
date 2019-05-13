22076dbf1fc0a6f6b06d4abfd21f03d3fddb30e6
Usheninte <jrandangana@gmail.com>
2019-05-13 15:27:11 +0100

    removed css

--- dist/index.js
+++ dist/index.js
@@ -10,8 +10,6 @@ var _react2 = _interopRequireDefault(_react);
 
 var _reactHelmet = require('react-helmet');
 
-require('./styles.css');
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var ReactFCCtest = function ReactFCCtest() {
